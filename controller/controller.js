1454367dbfbfb92510f130d8d4c643f78e9d03e3
Akash Ravi <akashkravi@gmail.com>
2018-01-07 16:42:24 +0530

    intial commit
    Should work fine as long as no exceptions/reloads are made.

---
+++ controller/controller.js
@@ -0,0 +1,110 @@
+var models = require('../model/model.js');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+
+
+module.exports = function (app,io){
+    app.use( bodyParser.json() );
+    app.use(bodyParser.urlencoded({     
+        extended: true
+    }));
+    
+    app.get('/',function(req,res){
+        res.sendFile(path.resolve(__dirname+"/../views/index.html"));
+    });
+    
+    app.post('/register',function(req,res){
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader("Access-Control-Allow-Method","'GET, POST, OPTIONS, PUT, PATCH, DELETE'");
+        var user={
+            "name":req.body.name,
+            "handle":req.body.handle,
+            "password":req.body.password,
+            "phone":req.body.phone,
+            "email":req.body.email,
+        };
+        console.log(user);
+        
+        models.user.findOne({"handle":req.body.handle},function(err,doc){
+            if(err){
+                res.json(err); 
+            }
+            if(doc == null){
+                models.user.create(user,function(err,doc){
+                    if(err) res.json(err);
+                    else{
+                        res.send("success");
+                    }
+                });
+            }else{
+                res.send("User already found");
+            }
+        })
+        
+    });
+    
+    
+    var handle=null;
+    var private=null;
+    var users={};
+    var keys={};
+    
+    app.post('/login',function(req,res){
+        console.log(req.body.handle);
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader("Access-Control-Allow-Method","'GET, POST, OPTIONS, PUT, PATCH, DELETE'");
+        handle = req.body.handle;
+        models.user.findOne({"handle":req.body.handle, "password":req.body.password},function(err,doc){
+            if(err){
+                res.send(err); 
+            }
+            if(doc==null){
+                res.send("User has not registered");
+            }
+            else{
+                console.log("Asas"+__dirname);
+//                res.sendFile(path.resolve(__dirname+"/../views/chat1.html"));
+                res.send("success");
+            }
+            
+    });
+    });
+    
+    io.on('connection',function(socket){
+        console.log("Connection :User is connected  "+handle);
+        console.log("Connection : " +socket.id);
+        io.to(socket.id).emit('handle', handle);
+        users[handle]=socket.id;
+        keys[socket.id]=handle;
+        console.log("Users list : "+users);
+        console.log("keys list : "+keys);
+        
+        
+        
+        socket.on('group message',function(msg){
+            console.log(msg);
+            io.emit('group',msg);
+        });
+        
+        socket.on('private message',function(msg){
+            console.log('message  :'+msg.split("#*@")[0]);
+            models.messages.create({
+                "message":msg.split("#*@")[1],
+                "sender" :msg.split("#*@")[2],
+                "reciever":msg.split("#*@")[0],
+                "date" : new Date()});
+            io.to(users[msg.split("#*@")[0]]).emit('private message', msg);
+        });
+        
+        socket.on('disconnect', function(){
+            delete users[keys[socket.id]];
+            delete keys[socket.id];
+            io.emit('users',users);
+            console.log(users);
+        });
+    });
+    
+    
+    
+}
